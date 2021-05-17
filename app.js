0ff48edbe1c766b4855e147a013452a69c6b6d71
ANYTIME APPLIANCE <anytimeappliance@ANYTIMEs-Mac-mini.local>
2021-05-17 15:23:58 -0400

    Info appears on page

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const button = document.querySelector('#search-btn')
 // create nameless function with axios and the url
 const getParkNames = async (input) => {
   try {
-    const filterParks = await axios.get(`https://developer.nps.gov/api/v1/parks?stateCode=${input}&api_key=iDGndNNbXpQ40TKBbBOEIetSM80ONg1kacWC9Pvn`)
+    let filterParks = await axios.get(`https://developer.nps.gov/api/v1/parks?stateCode=${input}&api_key=iDGndNNbXpQ40TKBbBOEIetSM80ONg1kacWC9Pvn`)
     const parkData = filterParks.data.data
     console.log(parkData)
+    // showParks(parkData)
   } catch (error) {
     console.error(error)
   }
@@ -18,11 +19,9 @@ function showParks(parkData) {
     <h2>${park.fullName}</h2>
     <img src="${park.images}">
     <p>${park.description}</hp>
-    <h3>${park.operatinghours.standardhours}</h3>
-    <h3>${park.address}</h3>
     `
-    document.querySelector('.park-data').insertAdjacentElement('beforeend', parkInfo)
-    return parkInfo
+    document.querySelector('.park-data').insertAdjacentHTML('beforeend', parkInfo)
+    // return parkInfo
   })
 }
 
@@ -34,8 +33,12 @@ button.addEventListener('click', () => {
 
 // append the info to the DOM^
 function appendParkInfo() {
-  let displayPark = document.querySelector('.park-data')
+  let displayPark = document.querySelector('section')
   displayPark.append(showParks)
 }
 // Remove recently searched from the DOM
 // define button 
+
+// going back into the code
+// <h3>${park.operatinghours.standardhours}</h3>
+// <h3>${park.address[0].city}</h3>
